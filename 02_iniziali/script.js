51280e763a3211a72a00b097625466368539b21f
AndreaCatapano <a.catapano96@gmail.com>
2025-03-03 15:07:37 +0100

    feat: completed "iniziali"

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,37 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function lookForInitials (names) {
+    const initials = [];
+    for (let i = 0; i < names.length; i++){
+        const name = names[i];
+         initials.push(name[0]);
+    }
+    return initials;
+}
+
+ const lookForInitialsHarrow = names => {
+    const initials = [];
+    for (let i = 0; i < names.length; i++){
+        const name = names[i];
+         initials.push(name[0]);
+    }
+    return initials;
+}
+
+function lookForInitialsMap(names) {
+    return names.map(name => name[0]);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const namesInitialis = lookForInitials(names);
+console.log(namesInitialis)
 
+const namesInitialisHarrow = lookForInitialsHarrow(names);
+console.log(namesInitialisHarrow)
+
+const namesInitialisMap = lookForInitialsMap(names);
+console.log(namesInitialisMap)
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
