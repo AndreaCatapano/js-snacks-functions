2542b394d64116300872823943f9b9d4a1e5b007
AndreaCatapano <a.catapano96@gmail.com>
2025-03-03 15:26:16 +0100

    feat: completed "filtra iniziali"

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,45 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function searchInitials(names, filter) {
+    const namesFiltered = [];
+    const filterUpperCase = filter.toUpperCase();
+    for (let i = 0; i < names.length; i++){
+        const name = names[i];
+         if (name[0].toUpperCase() === filterUpperCase){
+            namesFiltered.push(name);
+         }
+    }
+    return namesFiltered;
+}
+
+const searchInitialsHarrow = (names, filter) => {
+    const namesFiltered = [];
+    const filterUpperCase = filter.toUpperCase();
+    for (let i = 0; i < names.length; i++){
+        const name = names[i];
+         if (name[0].toUpperCase() === filterUpperCase){
+            namesFiltered.push(name);
+         }
+    }
+    return namesFiltered;
+}
+
+
+function searchInitialsFilter(names, filter) {
+    const filterUpperCase = filter.toUpperCase();
+    return names.filter(name => name[0].toUpperCase() === filterUpperCase);
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
+const namesWhitA = searchInitials(names, "a");
+console.log(namesWhitA);
 
 
+console.log(searchInitialsHarrow(names, "l"));
+
+
+console.log(searchInitialsFilter(names, "m"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
